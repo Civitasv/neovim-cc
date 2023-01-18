61a88003ef7e6c260d42c93307690f8c26fe9108
Civitasv <hscivitasv@gmail.com>
2023-01-18 19:44:30 +0800

    add some tests

--- gen_api.py
+++ gen_api.py
@@ -2,74 +2,87 @@
 # -*- coding: utf-8 -*-
 
 from jinja2 import Environment, FileSystemLoader
-import msgpack, sys, os, subprocess
+import msgpack
+import os
+import subprocess
 import re
 
-class InvalidType(Exception): 
+
+class InvalidType(Exception):
     pass
 
+
 class NativeType:
-    def __init__(self, name, expect_ref = False):
+    def __init__(self, name, expect_ref=False):
         self.name = name
         self.expect_ref = expect_ref
 
-REMAP_T = {
-    'ArrayOf(Integer, 2)': NativeType('std::vector<Integer>', True),
-    'Boolean': NativeType('bool'),
-    'String': NativeType('std::string', True),
 
-    'void': NativeType('void'),
-    'Window': NativeType('Window'),
-    'Buffer': NativeType('Buffer'),
-    'Tabpage': NativeType('Tabpage'),
-    'Integer': NativeType('Integer'),
-    'Object': NativeType('Object', True)
+REMAP_T = {
+    "ArrayOf(Integer, 2)": NativeType("std::vector<Integer>", True),
+    "Boolean": NativeType("bool"),
+    "String": NativeType("std::string", True),
+    "void": NativeType("void"),
+    "Window": NativeType("Window"),
+    "Buffer": NativeType("Buffer"),
+    "Tabpage": NativeType("Tabpage"),
+    "Integer": NativeType("Integer"),
+    "Object": NativeType("Object", True),
 }
 
+
 def convert_type_to_native(nvim_t, enable_ref_op):
-    array_of = r'ArrayOf\(\s*(\w+)\s*\)'
-    
+    array_of = r"ArrayOf\(\s*(\w+)\s*\)"
+
     obj = re.match(array_of, nvim_t)
     if obj:
-        ret = 'std::vector<%s>' % convert_type_to_native(obj.groups()[0], False)
-        return 'const ' + ret + '&' if enable_ref_op else ret
-    
+        ret = "std::vector<%s>" % convert_type_to_native(obj.groups()[0], False)
+        return "const " + ret + "&" if enable_ref_op else ret
+
     if nvim_t in REMAP_T:
         native_t = REMAP_T[nvim_t]
-        return 'const ' + native_t.name + '&' if enable_ref_op and native_t.expect_ref else native_t.name
+        return (
+            "const " + native_t.name + "&"
+            if enable_ref_op and native_t.expect_ref
+            else native_t.name
+        )
     else:
-        print "unknown nvim type name: " + str(nvim_t)
+        print("unknown nvim type name: " + str(nvim_t))
         raise InvalidType()
-    
-    #TODO: implement error handler
-    #return nvim_t
+
+    # TODO: implement error handler
+    # return nvim_t
+
 
 def main():
-    env = Environment(loader=FileSystemLoader('templates', encoding='utf8'))
-    tpl = env.get_template('nvim.hpp')
+    env = Environment(loader=FileSystemLoader("templates", encoding="utf8"))
+    tpl = env.get_template("nvim.hpp")
 
-    api_info = subprocess.check_output(["nvim", '--api-info'])
+    api_info = subprocess.check_output(["nvim", "--api-info"])
     unpacked_api = msgpack.unpackb(api_info)
 
     functions = []
-    for f in unpacked_api['functions']:
-
+    for f in unpacked_api["functions"]:
         d = {}
-        d['name'] = f['name']
+        d["name"] = f["name"]
 
         try:
-            d['return'] = convert_type_to_native(f['return_type'], False)
-            d['args'] = [{'type': convert_type_to_native(arg[0], True), 'name': arg[1]} for arg in f['parameters']]
+            d["return"] = convert_type_to_native(f["return_type"], False)
+            d["args"] = [
+                {"type": convert_type_to_native(arg[0], True), "name": arg[1]}
+                for arg in f["parameters"]
+            ]
             functions.append(d)
-        except InvalidType as e:
-            print "invalid function = " + str(f)
+        except InvalidType:
+            print("invalid function = " + str(f))
 
-    api = tpl.render({'functions': functions})
-    #print api.encode('utf-8')
+    print("FUNCTIONS", functions)
+    api = tpl.render({"functions": functions})
+    # print api.encode('utf-8')
 
-    with open(os.path.join("./gen", "nvim.hpp"), 'w') as f:
+    with open(os.path.join("./gen", "nvim.hpp"), "w") as f:
         f.write(api)
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
--- include/socket.hpp
+++ include/socket.hpp
@@ -3,8 +3,8 @@
 #define BOOST_VARIANT_USE_RELAXED_GET_BY_DEFAULT
 
 #include <boost/asio/deadline_timer.hpp>
-#include <boost/asio/io_service.hpp>
 #include <boost/asio/generic/stream_protocol.hpp>
+#include <boost/asio/io_service.hpp>
 #include <boost/asio/read_until.hpp>
 #include <boost/asio/streambuf.hpp>
 #include <boost/system/system_error.hpp>
@@ -14,32 +14,29 @@
 
 namespace nvim {
 
-//TODO Implement asynchronous response handler.
+// TODO Implement asynchronous response handler.
 class Socket {
-public:
-    Socket() : 
-        socket_(io_service_),
-        deadline_(io_service_)
-    {
-        deadline_.expires_at(boost::posix_time::pos_infin);
-        check_deadline();
-    }
+ public:
+  Socket() : socket_(io_service_), deadline_(io_service_) {
+    deadline_.expires_at(boost::posix_time::pos_infin);
+    check_deadline();
+  }
 
-    void connect_tcp(const std::string& host, 
-               const std::string& service, double timeout_sec);
+  void connect_tcp(const std::string &host, const std::string &service,
+                   double timeout_sec);
 
-    size_t read(char *rbuf, size_t capacity, double timeout_sec);
-    void write(char *sbuf, size_t size, double timeout_sec);
+  size_t read(char *rbuf, size_t capacity, double timeout_sec);
+  void write(char *sbuf, size_t size, double timeout_sec);
 
-private:
-    void check_deadline();
+ private:
+  void check_deadline();
 
-    boost::asio::io_service io_service_;
-    boost::asio::generic::stream_protocol::socket socket_;
-    boost::asio::deadline_timer deadline_;
-    boost::asio::streambuf input_buffer_;
+  boost::asio::io_service io_service_;
+  boost::asio::generic::stream_protocol::socket socket_;
+  boost::asio::deadline_timer deadline_;
+  boost::asio::streambuf input_buffer_;
 };
 
-} //namespace nvim
+}  // namespace nvim
 
-#endif //NEOVIM_CPP__SOCKET_HPP_
+#endif  // NEOVIM_CPP__SOCKET_HPP_
